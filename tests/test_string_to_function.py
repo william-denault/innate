385326db9e04616c5c41b6c0fc3260f6bcb7b9c5
william-denault <willam.denault@gmail.com>
2024-06-27 11:56:44 +0200

    working test

--- tests/test_string_to_function.py
+++ tests/test_string_to_function.py
@@ -6,7 +6,7 @@ from innate.regression.methods import extract_coef_names
 from innate.regression.methods import extract_variables_names   
 from innate.regression.methods import create_coef_dict
 
-
+ 
 
 
 expression = 'a + b / (variable1_range/10000.0) + c * np.log10(variable2_range/10000)'
@@ -32,11 +32,20 @@ expression = 'a + b / (variable1_range/10000.0) + c * np.log10(variable2_range/1
 coefficients = {'a': 1, 'b': 4, 'c': 7}
 
 # Specify the variable names that should vary
-variable_names = ['variable1', 'variable2']  # These names should match usage in expression
+variable_names = ['variable1_range', 'variable2_range']  # These names should match usage in expression
 
 # Create a function that varies with specified variables
-specific_function = generate_specific_function(expression, coefficients, variable_names) 
+@pytest.fixture 
+def specific_function():
+    expression = 'a + b / (variable1_range/10000.0) + c * np.log10(variable2_range/10000)'
 
+    # Fixed values for a, b, c
+    coefficients = {'a': 1, 'b': 4, 'c': 7}
+
+    # Specify the variable names that should vary
+    variable_names = ['variable1_range', 'variable2_range']
+    return generate_specific_function(expression, coefficients, variable_names) 
+#print( str(specific_function([10000, 20000, 30000],[10000, 20000, 30000])))
 @pytest.mark.parametrize(
     "test, expected",
     [
@@ -47,9 +56,11 @@ specific_function = generate_specific_function(expression, coefficients, variabl
 
 )
 def test_specific_function(test, expected):
+    specific_function = generate_specific_function(expression, coefficients, variable_names) 
     np.testing.assert_almost_equal(
-        specific_function(np.array(test),np.array(test)), np.array(expected), decimal=7
+        specific_function([10000, 20000, 30000],[10000, 20000, 30000]), np.array(expected), decimal=7
     )
+
 
 expression = 'a + b / (variable1_range/10000.0) + c * np.log10(variable2_range/10000)'
 
@@ -80,8 +91,8 @@ expression = 'a + b / (variable1/10000.0) + c * np.log10(variable2/10000)'
 )
 def test_extract_coef_names(test, expected):
     np.testing.assert_equal(
-        extract_coef_names( test ,  expected) 
-    )
+        extract_coef_names( test ),  expected) 
+ 
 
 
 
@@ -93,8 +104,11 @@ def test_extract_coef_names(test, expected):
 
 )
 def test_create_coef_dict(test, expected):
+    expression = 'a + b / (variable1_range/10000.0) + c * np.log10(variable2_range/10000)'
+    coef_names = extract_coef_names(expression)
+    coef_values = [1,2,3]
     np.testing.assert_equal(
-       create_coef_dict(test[1],test[2] )
+       create_coef_dict(coef_names,coef_values),expected
     )
 
 @pytest.mark.parametrize(
@@ -106,7 +120,7 @@ def test_create_coef_dict(test, expected):
 )
 def test_extract_variables_names(test, expected):
     np.testing.assert_equal(
-       extract_variables_names(test )
+       extract_variables_names(test ), expected
     )
 
 
